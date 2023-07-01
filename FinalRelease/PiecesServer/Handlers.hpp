8922b11c83fb8c971fcf129b1df17efdab7a327b
joseph.valverdekong <joseph.valverdekong@ucr.ac.cr>
2023-06-30 20:01:24 -0600

    solved merge issues

--- FinalRelease/PiecesServer/Handlers.hpp
+++ FinalRelease/PiecesServer/Handlers.hpp
@@ -23,8 +23,32 @@ class UDPHandler : public Handler<std::shared_ptr<std::vector<char>>> {
       std::shared_ptr<std::vector<char>> stopCondition) : Handler (consumingQueue, stopCondition){}
 
  private:
-  void handleSingle(std::pair<std::string, int> handlingSocket) {
-    std::cout << "ip address: " << handlingData->first <<
-        "\nport:" << handlingData->second << std::endl;
+  void handleSingle(std::shared_ptr<std::vector<char>> handlingData) {
+    LegoMessageCode code = (LegoMessageCode) 0;
+
+    // get the code from the message
+    memcpy(&code, (void*) handlingData->data(), sizeof(LegoMessageCode));
+    
+    std::string buffer;
+
+    buffer.resize(handlingData->size() - 5);
+
+    memcpy(buffer.data(), &handlingData->data()[5], buffer.size());
+
+    // get the host
+    std::string ip = buffer.substr(
+        0, // from start
+        buffer.find(':') - 1 // until separator
+        );
+
+    // get the port
+    int port = std::stoi(buffer.substr(
+        buffer.find(':') + 1, // from after separator
+        buffer.size() // until end of message
+        ));
+
+    std::cout << "CODE: " << code
+        << "\n\tip address: " << ip
+        << "\n\tport:" << port << std::endl;
   }
 };
