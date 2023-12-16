84ec7fe0c8c1be8cb2412bf2d994d94b45a52713
Joseph Stuart Valverde Kong <112024060+joseph97svk@users.noreply.github.com>
2023-12-15 19:44:45 -0600

    creating-common-server: Added logging to HttpServer initialization and corrected Logger interface issue preventing const std::strings

--- HttpServer/Logger/ILogger.hpp
+++ HttpServer/Logger/ILogger.hpp
@@ -9,10 +9,10 @@ class ILogger {
 public:
     virtual ~ILogger() = default;
 
-    virtual void log(std::string& message) = 0;
-    virtual void warning(std::string& message) = 0;
-    virtual void error(std::string& message) = 0;
-    virtual void info(std::string& message) = 0;
+    virtual void log(std::string message) = 0;
+    virtual void warning(std::string message) = 0;
+    virtual void error(std::string message) = 0;
+    virtual void info(std::string message) = 0;
 };
 
 #endif //LEGO_FIGURE_MAKER_ILOGGER_HPP
